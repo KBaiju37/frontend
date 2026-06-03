bbd98f16496ae74dfe28556c8d1559460a927ec0
KBaiju37 <karvirbaijunath@gmail.com>
2026-06-03 15:31:23 +0530

    usage of js and using console in inspect, creating fucntions to manupulate

---
+++ p1.js
@@ -0,0 +1,14 @@
+console.log("hello")
+
+abc = document.getElementById("h")
+console.log(abc)
+
+abc.textcontent = "hello world"
+abc.style.background = "red"
+abc.style.color ="black"
+
+function hii(){
+    abc.textcontent = "hello world"
+    abc.style.background = "black"
+    abc.style.color = "yellow"
+}
