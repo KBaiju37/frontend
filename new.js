e92e60f9054205c44d46758d8e460609fdcbb379
KBaiju37 <karvirbaijunath@gmail.com>
2026-06-04 10:28:16 +0530

    how to use queryselector and querylistener

--- new.js
+++ new.js
@@ -1,1 +1,24 @@
- let line = document.getElementById("h")
+let line = document.getElementById("a");
+console.log(line)
+
+
+btn = document.querySelector("button");
+btn.addEventListener("click", ()=>{
+    if(document.querySelector("h1").textContent == "now i know dom manipulation"){
+        document.querySelector("h1").textContent = "javacript is really tricky"
+        document.querySelector("h1").style.color = "aqua";
+
+    } else {
+        document.querySelector("h1").textContent = "now i know dom manipulation";
+        document.querySelector("h1").style.color = "white";
+       
+    }
+
+
+
+
+
+})
+
+
+
